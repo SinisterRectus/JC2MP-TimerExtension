bb241c5db3e93f931c0f816d353c14b8fe23c284
SinisterRectus <SinisterRectus@users.noreply.github.com>
2016-04-11 14:26:19 -0400

    Delta time is now passed to callback calls

--- shared/Timer.lua
+++ shared/Timer.lua
@@ -1,40 +1,42 @@
 local events = {}
+local GetTime = Timer.GetMilliseconds
 
 function Timer.Clear(timer)
     events[timer] = nil
 end
 
-function Timer.SetImmediate(callback, ...)
+function Timer.SetImmediate(callback)
     local timer = Timer()
-    events[timer] = {1, callback, {...}}
+    events[timer] = {1, callback}
     return timer
 end
 
-function Timer.SetTimeout(delay, callback, ...)
+function Timer.SetTimeout(delay, callback)
     local timer = Timer()
-    events[timer] = {2, callback, {...}, delay}
+    events[timer] = {2, callback, delay}
     return timer
 end
 
-function Timer.SetInterval(delay, callback, ...)
+function Timer.SetInterval(delay, callback)
     local timer = Timer()
-    events[timer] = {3, callback, {...}, delay}
+    events[timer] = {3, callback, delay}
     return timer
 end
 
 function Timer.Tick()
     for timer, event in pairs(events) do
+        local delta = GetTime(timer)
         if event[1] == 1 then
-            event[2](table.unpack(event[3]))
+            event[2]({delta = delta})
             timer:Clear()
         elseif event[1] == 2 then
-            if timer:GetMilliseconds() > event[4] then
-                event[2](table.unpack(event[3]))
+            if delta >= event[3] then
+                event[2]({delta = delta})
                 timer:Clear()
             end
         elseif event[1] == 3 then
-            if timer:GetMilliseconds() > event[4] then
-                event[2](table.unpack(event[3]))
+            if delta >= event[3] then
+                event[2]({delta = delta})
                 timer:Restart()
             end
         end
